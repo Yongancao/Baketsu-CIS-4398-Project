09eb7ac53b4396ad60d5a8dc5748cba1f2d2f0bd
jzhao234 <jzhao1155@gmail.com>
2025-12-08 14:45:11 +0900

    navbar improvments

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -10,37 +10,36 @@ export default function Navbar() {
     const {isLoggedIn, logout} = useAuthStatus();
 
     return (
-        <nav className="fixed z-50 w-full flex items-center gap-10 pb-2 pt-2 bg-white dark:bg-[#151516] text-black dark:text-white">
+        <nav className="fixed z-50 w-full flex items-center pb-2 pt-2 bg-white dark:bg-[#151516] text-black dark:text-white justify-between">
 
-            <div className="flex pl-15 pr-15 pt-2 pb-2 text-3xl font-semibold tracking-tight">
+            <div className="flex items-center gap-7 pl-10 pr-15 pt-2 pb-2 ">
                 
-                <Link href="/" className="flex items-center">
+                <Link href="/" className="flex gap-4 items-center text-3xl font-semibold tracking-tight">
                     <Logo/>
                     Baketsu 
                 </Link>
-            </div>
-            <div className="flex gap-10">
-                <Link href="/" className="hover:underline"> Home </Link>                
+                            
             </div> 
             {isLoggedIn ? (
                 <>
+                    <Link href="/" className="hover:underline"> Home </Link>    
                     <Link href="/upload" className="hover:underline"> Upload </Link>
                     <Link href="/dashboard" className="hover:underline"> Dashboard </Link>
                     <Link href="/files" className="hover:underline"> Files </Link>
                     <button onClick={logout} className="hover:text-red-500">
                         Logout
                     </button>
+                    <div className="pl-10 pr-15"><DarkModeToggle/></div>
                 </>
             ) : (
                 <>
-                    <div className="flex">
+                    <div className="flex items-center gap-10">
                         <Link href="/register" className="hover:underline"> Register </Link>
-                        <p> / </p>
                         <Link href="/login" className="hover:underline"> Login </Link>
+                        <div className="pl-5 pr-15"><DarkModeToggle/></div>
                     </div>
                 </>
             )}
-            <DarkModeToggle/>
         </nav>
     )
 }
